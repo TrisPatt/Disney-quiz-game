9130e2796ae724840723862267d338449092d538
TrisPatt <tristanpattison@yahoo.co.uk>
2024-03-07 21:18:27 +0000

    add placeholder questions array in script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('quiz test');
+//create array of objects for questions
+const questions = [
+    {
+        question: "what is 2+2",
+        options: ["2", "3", "4", "5"],
+        answer: 2
+    },
+    {
+        question: "what is 1+4",
+        options: ["2","7","8","5"],
+        answer: 4
+    },
+    {
+        question: "what is 5x5",
+        options: ["20","25","30","40"],
+        answer: 2
+    }
+]
