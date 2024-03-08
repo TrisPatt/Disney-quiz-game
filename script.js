d07e341d39b997f47e48fdadc98ba0d2e9c9dec8
TrisPatt <tristanpattison@yahoo.co.uk>
2024-03-08 16:18:49 +0000

    fix checkanswer function in js

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ const questions = [
     {
         question: "what is 1+4",
         options: ["2","7","8","5"],
-        answer: 4
+        answer: 3
     },
     {
         question: "what is 5x5",
         options: ["20","25","30","40"],
-        answer: 2
-    }
-]
+        answer: 1
+    }];
 
 //variables to start questions and answers
 let currentQuestion = 0;
@@ -24,36 +23,24 @@ let correctAnswers = 0;
 //function to display the questions
 function displayQuestion() {
     const questionTxt = document.getElementById('the-questions');
-    questionTxt.textContent=questions[currentQuestion].question;
+    questionTxt.textContent = questions[currentQuestion].question;
 
     const options = document.querySelectorAll(".option");
     options.forEach((option, index) => {
         option.textContent = questions[currentQuestion].options[index];
     });
+
 }
 
-//function to check the answer and display prompt
-function checkAnswer(select) {
-    const result = document.getElementById("result");
-    if (select === questions[currentQuestion].answer) {
-        result.textContent = alert("correct");
-        correctAnswers++;
+
+//function to check the answer and display result
+function checkAnswer(checked) {
+    let answer = questions[currentQuestion].answer;
+    if (checked === answer) {
+        alert("correct!");
             }
     else {
-        result.textContent = alert("incorrect");
+        alert("incorrect");
     }
-
-    setTimeout(() => {
-        currentQuestion++;
-    
-    if (currentQuestion < questions.length) {
-        displayQuestion();}
-        else {
-            const gameArea = document.querySelector(".game-area");
-            game-gameArea.innerHTML = <p>you got
-            ${correctAnswers} out of ${questions.length}questions.</p>
-        }
-    });
 }
-
 displayQuestion();
